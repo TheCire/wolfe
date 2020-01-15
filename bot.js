e9b9978e4f0e0e5e757dad8934622d9255af5346
TheCire <TheCire@users.noreply.github.com>
2020-01-15 15:08:47 -0800

    bot now deletes reconized commands from chat to keep down on noise

--- bot.js
+++ bot.js
@@ -27,23 +27,28 @@ bot.on('ready', function (evt) {
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `/wel`
     logger.info(message);
     logger.info(channelID);
+    logger.info(evt.d.id);
     if (message.startsWith(config.commandprefix)) {
         var args = message.substring(0).split(' ');
         var cmd = args[1];
-        var  message = "";
+        var  responseMessage = "";
         logger.info('')
         logger.info(channelID);
         args = args.splice(1);
         const userInfo = bot.users[userID];
+        let msgID = evt.d.id
+        let msgObj = { channelID: channelID, messageID: msgID };
 
         switch(cmd) {
             // /wel ping
             case 'ping':
+                bot.deleteMessage(msgObj,(err,response)=>{ if(err) console.log(err); })
                 bot.sendMessage({
                     to: config.botRoom,
                     message: 'Pong!'
@@ -51,6 +56,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 break;
             case 'available':
                 if(!availablePlayers.includes(userInfo.username)) {
+                    bot.deleteMessage(msgObj,(err,response)=>{ if(err) console.log(err); })
                     availablePlayers.push(userInfo.username);
                     bot.sendMessage({
                         to: config.botRoom,
@@ -60,6 +66,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 break;
             case 'available?':
                     if(!tentativePlayers.includes(userInfo.username)) {
+                        bot.deleteMessage(msgObj,(err,response)=>{ if(err) console.log(err); })
                         tentativePlayers.push(userInfo.username);
                         bot.sendMessage({
                             to: config.botRoom,
@@ -69,6 +76,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     break;
             case 'unavailable':
                 if(availablePlayers.includes(userInfo.username)) {
+                    bot.deleteMessage(msgObj,(err,response)=>{ if(err) console.log(err); })
                     availablePlayers = availablePlayers.filter(e => e !== userInfo.username);
                     bot.sendMessage({
                         to: config.botRoom,
@@ -76,32 +84,34 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     });
                 }
                 if(tentativePlayers.includes(userInfo.username)) {
+                    bot.deleteMessage(msgObj,(err,response)=>{ if(err) console.log(err); })
                     tentativePlayers = tentativePlayers.filter(e => e !== userInfo.username);
                     bot.sendMessage({
                         to: config.botRoom,
-                        message: userInfo.username + ' is no longer tnentatively available to play.'
+                        message: userInfo.username + ' is no longer tentatively available to play.'
                     });
                 }
 
                 break;
             case 'list':
-                message = "There are currently no players available to play.";
+                bot.deleteMessage(msgObj,(err,response)=>{ if(err) console.log(err); })
+                responseMessage = "There are currently no players available to play.";
                 if(availablePlayers.length > 0) {
-                    message = "There is/are currently " + availablePlayers.length + " player(s) available to play. The list of available players is/are: " + availablePlayers.join(", ");
+                    responseMessage = "There is/are currently " + availablePlayers.length + " player(s) available to play. The list of available players is/are: " + availablePlayers.join(", ");
                 }
                 if(tentativePlayers.length > 0) {
-                    message += "\nThere is/are currently " + tentativePlayers.length + " player(s) tentatively available to play. The list of tentative players is/are: " + tentativePlayers.join(", ");
+                    responseMessage += "\nThere is/are currently " + tentativePlayers.length + " player(s) tentatively available to play. The list of tentative players is/are: " + tentativePlayers.join(", ");
                 }
                 bot.sendMessage({
                     to: config.botRoom,
-                    message: message
+                    message: responseMessage
                 });
                 break;
             case 'help':
-                message = "please see " + config.helpURL + " for information on how to use this bot.";
+                responseMessage = "please see " + config.helpURL + " for information on how to use this bot.";
                 bot.sendMessage({
                     to: config.botRoom,
-                    message: message
+                    message: responseMessage
                 });
                 break;
             default:
@@ -109,3 +119,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
          }
      }
 });
+
+function logObject(obj) {
+    for (var property in obj) {
+        console.log(property);
+    }
+}
